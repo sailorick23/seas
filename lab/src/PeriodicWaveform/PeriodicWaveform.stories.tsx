f9d03b3fa73b0589f4dd6f0a8e8386584b406b68
sailorick <sailorick23@gmail.com>
2020-04-27 15:40:06 -0700

    lab(periodic-waveform/basic): setup canvas

--- lab/src/PeriodicWaveform/PeriodicWaveform.stories.tsx
+++ lab/src/PeriodicWaveform/PeriodicWaveform.stories.tsx
@@ -1,8 +1,17 @@
+import React, { useEffect, useRef } from 'react'
+
 export default {
   title: 'PeriodicWaveform',
   component: () => null,
 }
 
 export const Basic = () => {
-  return null
+  const canvasRef = useRef<HTMLCanvasElement | null>(null)
+  useEffect(() => {
+    const graphicsContext = canvasRef.current?.getContext('2d')
+    if (graphicsContext) {
+      const targetCanvas = graphicsContext.canvas
+    }
+  }, [])
+  return <canvas ref={canvasRef} width={256} height={256} />
 }
